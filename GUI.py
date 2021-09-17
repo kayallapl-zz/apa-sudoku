e322151acb96c0cd4905a4deef73a1d1c362904c
Kayalla <kayallapl@id.uff.br>
2021-09-17 16:25:31 -0300

    troca de dificuldades

--- GUI.py
+++ GUI.py
@@ -1,7 +1,11 @@
 # import pygame library
 import pygame
 import time
+import copy
 from pygame.constants import KEYDOWN
+import random
+
+from boards import *
  
 # initialise the pygame font
 pygame.font.init()
@@ -20,32 +24,9 @@ play = False
 interactive = False
 dif = 500 / 9
 fnt = pygame.font.SysFont("comicsans", 40)
-final_time = fnt.render("Time: " + "0.0", 1, (0,0,0))
-
-# Default Sudoku Board.
-grid = [
-        [7, 8, 0, 4, 0, 0, 1, 2, 0],
-        [6, 0, 0, 0, 7, 5, 0, 0, 9],
-        [0, 0, 0, 6, 0, 1, 0, 7, 8],
-        [0, 0, 7, 0, 4, 0, 2, 6, 0],
-        [0, 0, 1, 0, 5, 0, 9, 3, 0],
-        [9, 0, 4, 0, 6, 0, 0, 0, 5],
-        [0, 7, 0, 3, 0, 0, 0, 1, 2],
-        [1, 2, 0, 0, 0, 7, 4, 0, 0],
-        [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    ]
-
-grid_original = [
-        [7, 8, 0, 4, 0, 0, 1, 2, 0],
-        [6, 0, 0, 0, 7, 5, 0, 0, 9],
-        [0, 0, 0, 6, 0, 1, 0, 7, 8],
-        [0, 0, 7, 0, 4, 0, 2, 6, 0],
-        [0, 0, 1, 0, 5, 0, 9, 3, 0],
-        [9, 0, 4, 0, 6, 0, 0, 0, 5],
-        [0, 7, 0, 3, 0, 0, 0, 1, 2],
-        [1, 2, 0, 0, 0, 7, 4, 0, 0],
-        [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    ]
+final_time = fnt.render("Time: " + " 0.0", 1, (0,0,0))
+dificulty = 2
+random_int = random.randint(0,2)
  
 # Load test fonts for future use
 font1 = pygame.font.SysFont("comicsans", 40)
@@ -88,12 +69,7 @@ def draw():
         play_time = round(time.time() - start)
         text = fnt.render("Time: " + format_time(play_time), 1, (0,0,0))
         final_time = text
-        screen.blit(text, (540 - 200, 580))
- 
-# Raise error when wrong value entered
-def raise_error():
-    text1 = font1.render("WRONG !!!", 1, (0, 0, 0))
-    screen.blit(text1, (20, 570))
+        screen.blit(text, (540 - 180, 585))
  
 # Check if the value entered in board is valid
 def valid(board, i, j, value):
@@ -164,30 +140,55 @@ def update_screen():
 def feedback(it, linha, coluna, motivo):
     frase1 = "Tentativa de posicionar o número " + str(it)
     frase2 = "na linha " + str(linha+1) +", coluna " + str(coluna+1) + ", "
-    frase3 = "porém há um mesmo número na mesma " + str(motivo)
+    frase3 = "porém há um mesmo número na mesma " + str(motivo) + "."
     text1 = font2.render(frase1, 1, (0, 0, 0))
     text2 = font2.render(frase2, 1, (0, 0, 0))
     text3 = font2.render(frase3, 1, (0, 0, 0))
-    screen.blit(text1, (20, 520))       
-    screen.blit(text2, (20, 540))
-    screen.blit(text3, (20, 560))
+    screen.blit(text1, (15, 515))       
+    screen.blit(text2, (15, 530))
+    screen.blit(text3, (15, 545))
  
 # Display instruction for the game
 def instruction():
-    text1 = font2.render("Pressione D para reiniciar", 1, (0, 0, 0))
+    text1 = font2.render("Pressione R para reiniciar", 1, (0, 0, 0))
     text2 = font2.render("Pressione 1 para solução automática", 1, (0, 0, 0))
     text3 = font2.render("Pressione 2 para solução interativa", 1, (0, 0, 0))
-    screen.blit(text1, (20, 520))       
-    screen.blit(text2, (20, 540))
-    screen.blit(text3, (20, 560))
+    text4 = font2.render("Pressione D para trocar a dificuldade", 1, (0, 0, 0))
+    screen.blit(text1, (15, 515))       
+    screen.blit(text2, (15, 530))
+    screen.blit(text3, (15, 545))
+    screen.blit(text4, (15, 560))
  
 # Display options when solved
 def result():
     text1 = font1.render("FINISHED", 1, (0, 0, 0))
-    screen.blit(text1, (20, 580))   
+    screen.blit(text1, (15, 585))
+
+def render_difficulty():
+    if dificulty == 2:
+        string = "MÉDIA"
+        color = (0, 80, 255)
+    elif dificulty == 3:
+        string = "DIFÍCIL"
+        color = (255, 0, 0)
+    else:
+        string = "FÁCIL"
+        color = (0, 200, 0)
+    text1 = font2.render(str("Dificuldade: " + string), 1, color)
+    screen.blit(text1, (540 - 175, 515))
 
 # The loop thats keep the window running
 while run:
+    if dificulty == 1:
+        grid = copy.deepcopy(easy[random_int])
+        grid_original = copy.deepcopy(easy[random_int])
+    elif dificulty == 2:
+        grid = copy.deepcopy(intermediary[random_int])
+        grid_original = copy.deepcopy(intermediary[random_int])
+    else: 
+        grid = copy.deepcopy(hard[random_int])
+        grid_original = copy.deepcopy(hard[random_int])
+
     # White color background
     screen.fill((255, 255, 255))
     draw()
@@ -204,7 +205,7 @@ while run:
             if event.key == pygame.K_2:
                 play = True
                 interactive = True
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_r:
                 displayResult = False
                 hasError = False
                 play = False
@@ -219,6 +220,12 @@ while run:
                     [1, 2, 0, 0, 0, 7, 4, 0, 0],
                     [0, 4, 9, 2, 0, 6, 0, 0, 7]
                 ]
+            if event.key == pygame.K_d:
+                random_int = random.randint(0,2)
+                if dificulty < 3:
+                    dificulty += 1
+                else:
+                    dificulty = 1
     if play:
         start = time.time()
         if solve(grid, 0, 0) == False:
@@ -226,17 +233,15 @@ while run:
         else:
             displayResult = True
         play = False
-       
-    if hasError:
-        raise_error() 
 
     if displayResult:
         result()    
            
     draw()
     instruction()
+    render_difficulty()
     if not interactive:
-        screen.blit(final_time, (540 - 200, 580))
+        screen.blit(final_time, (540 - 180, 585))
  
     # Update window
     pygame.display.update() 
